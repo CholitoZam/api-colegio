729eb352efedd251d26e94c375ccb352122187e3
CholitoZam <108442141+CholitoZam@users.noreply.github.com>
2022-07-11 17:10:56 -0500

    Update AsistenciaController.java

--- src/main/java/apiconsultadatos/apiconsultadatos/controller/AsistenciaController.java
+++ src/main/java/apiconsultadatos/apiconsultadatos/controller/AsistenciaController.java
@@ -8,7 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import sun.util.resources.LocaleData;
+import java.util.resources.LocaleData;
 
 import java.io.IOException;
 import java.text.DateFormat;
